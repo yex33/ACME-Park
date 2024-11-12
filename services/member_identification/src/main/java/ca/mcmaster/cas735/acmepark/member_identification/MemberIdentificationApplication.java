17c3f78cb6969204d5cf1e903f7e61427c4f41ed
Leonard Chan <wxclx98@gmail.com>
2024-11-12 18:19:08 -0500

    Add member_identification boilerplate

---
+++ services/member_identification/src/main/java/ca/mcmaster/cas735/acmepark/member_identification/MemberIdentificationApplication.java
@@ -0,0 +1,13 @@
+package ca.mcmaster.cas735.acmepark.member_identification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemberIdentificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemberIdentificationApplication.class, args);
+	}
+
+}
---
+++ services/member_identification/src/test/java/ca/mcmaster/cas735/acmepark/member_identification/MemberIdentificationApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.mcmaster.cas735.acmepark.member_identification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemberIdentificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
