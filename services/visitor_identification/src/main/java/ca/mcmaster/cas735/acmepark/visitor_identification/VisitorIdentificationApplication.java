039ac008960c9eb598a02e0dab7ad8fa9492c438
Leonard Chan <wxclx98@gmail.com>
2024-11-22 00:24:43 -0500

    Starting point of visitor identification

---
+++ services/visitor_identification/src/main/java/ca/mcmaster/cas735/acmepark/visitor_identification/VisitorIdentificationApplication.java
@@ -0,0 +1,13 @@
+package ca.mcmaster.cas735.acmepark.visitor_identification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisitorIdentificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VisitorIdentificationApplication.class, args);
+	}
+
+}
---
+++ services/visitor_identification/src/test/java/ca/mcmaster/cas735/acmepark/visitor_identification/VisitorIdentificationApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.mcmaster.cas735.acmepark.visitor_identification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VisitorIdentificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
