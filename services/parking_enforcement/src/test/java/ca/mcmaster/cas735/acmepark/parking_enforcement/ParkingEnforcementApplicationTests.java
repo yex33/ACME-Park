0850ac677a9a62525f3185020ad02a74efb5de00
Xunzhou Ye <yex33@mcmaster.ca>
2024-11-27 18:21:04 -0500

    started on testing

--- services/parking_enforcement/src/test/java/ca/mcmaster/cas735/acmepark/parking_enforcement/ParkingEnforcementApplicationTests.java
+++ services/parking_enforcement/src/test/java/ca/mcmaster/cas735/acmepark/parking_enforcement/ParkingEnforcementApplicationTests.java
@@ -1,10 +1,20 @@
 package ca.mcmaster.cas735.acmepark.parking_enforcement;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.stream.binder.test.InputDestination;
+import org.springframework.cloud.stream.binder.test.OutputDestination;
+import org.springframework.cloud.stream.binder.test.TestChannelBinderConfiguration;
+import org.springframework.context.annotation.Import;
 
 @SpringBootTest
+@Import(TestChannelBinderConfiguration.class)
 class ParkingEnforcementApplicationTests {
+    @Autowired
+    private InputDestination inputDestination;
+    @Autowired
+    private OutputDestination outputDestination;
 
     @Test
     void contextLoads() {
