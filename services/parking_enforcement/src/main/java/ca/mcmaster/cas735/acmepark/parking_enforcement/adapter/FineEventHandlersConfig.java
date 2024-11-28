90e304f2c039dbe4f0196195adc7c6794b3de27c
Xunzhou Ye <yex33@mcmaster.ca>
2024-11-27 20:56:53 -0500

    add test exchange to make sure cloud stream works

--- services/parking_enforcement/src/main/java/ca/mcmaster/cas735/acmepark/parking_enforcement/adapter/FineEventHandlersConfig.java
+++ services/parking_enforcement/src/main/java/ca/mcmaster/cas735/acmepark/parking_enforcement/adapter/FineEventHandlersConfig.java
@@ -6,7 +6,10 @@ import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
 import java.util.function.Consumer;
+import java.util.function.Supplier;
 
 @AllArgsConstructor
 @Configuration
@@ -16,5 +19,13 @@ public class FineEventHandlersConfig {
     @Bean
     public Consumer<FineEvent> fineEventConsumer() {
         return fineManagement::registerFine;
+    }
+
+    @Bean
+    public Supplier<FineEvent> fineEventSupplier() {
+        return () -> FineEvent.builder()
+                .userId(UUID.randomUUID())
+                .violation("you should not park")
+                .issuedOn(LocalDateTime.now()).build();
     }
 }
